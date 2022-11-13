7dfccad83046ce3ea19cce39326437dc8bafb996
marjanajenia <marjanajenia98@gmail.com>
2022-11-13 23:37:31 +0600

    backend mastering

---
+++ resources/views/backend/includes/sidebar.blade.php
@@ -0,0 +1,118 @@
+<!-- ########## START: LEFT PANEL ########## -->
+<div class="br-logo"><a href=""><span>[</span>bracket <i>plus</i><span>]</span></a></div>
+    <div class="br-sideleft sideleft-scrollbar">
+      <label class="sidebar-label pd-x-10 mg-t-20 op-3">Navigation</label>
+      <ul class="br-sideleft-menu">
+        <li class="br-menu-item">
+          <a href=" {{ Route('dashboard') }}" class="br-menu-link active">
+            <i class="menu-item-icon icon ion-ios-home-outline tx-24"></i>
+            <span class="menu-item-label">Dashboard</span>
+          </a><!-- br-menu-link -->
+        </li><!-- br-menu-item -->
+        <li class="br-menu-item">
+          <a href="mailbox.html" class="br-menu-link">
+            <i class="menu-item-icon icon ion-ios-email-outline tx-24"></i>
+            <span class="menu-item-label">Mailbox</span>
+          </a><!-- br-menu-link -->
+        </li><!-- br-menu-item -->
+        <label class="sidebar-label pd-x-10 mg-t-25 mg-b-20 tx-info">Admin</label>
+        <!-------------------------- Seller -------------------------->
+        <li class="br-menu-item">
+          <a href="#" class="br-menu-link with-sub">
+            <i class="menu-item-icon icon ion-ios-photos-outline tx-20"></i>
+            <span class="menu-item-label">Seller</span>
+          </a><!-- br-menu-link -->
+          <ul class="br-menu-sub">
+            <li class="sub-item"><a href="" class="sub-link">Manage Seller</a></li>  
+            <li class="sub-item"><a href="" class="sub-link">Add Seller</a></li>              
+          </ul>
+        </li>
+        <!-------------------------- Category -------------------------->
+        <li class="br-menu-item">
+          <a href="#" class="br-menu-link with-sub">
+            <i class="menu-item-icon icon ion-ios-photos-outline tx-20"></i>
+            <span class="menu-item-label">Category</span>
+          </a><!-- br-menu-link -->
+          <ul class="br-menu-sub">
+            <li class="sub-item"><a href="" class="sub-link">Manage Category</a></li>  
+            <li class="sub-item"><a href="" class="sub-link">Add Category</a></li>              
+          </ul>
+        </li>
+        <!-------------------------- Sub Category -------------------------->
+        <li class="br-menu-item">
+          <a href="#" class="br-menu-link with-sub">
+            <i class="menu-item-icon icon ion-ios-photos-outline tx-20"></i>
+            <span class="menu-item-label">Sub Category</span>
+          </a><!-- br-menu-link -->
+          <ul class="br-menu-sub">
+            <li class="sub-item"><a href="" class="sub-link">Manage Sub-Category</a></li>  
+            <li class="sub-item"><a href="" class="sub-link">Add Sub-Category</a></li>              
+          </ul>
+        </li>
+        <!-------------------------- product -------------------------->
+        <li class="br-menu-item">
+          <a href="#" class="br-menu-link with-sub">
+            <i class="menu-item-icon icon ion-ios-photos-outline tx-20"></i>
+            <span class="menu-item-label">Product</span>
+          </a><!-- br-menu-link -->
+          <ul class="br-menu-sub">
+            <li class="sub-item"><a href="" class="sub-link">Manage Product</a></li>  
+            <li class="sub-item"><a href="" class="sub-link">Add Product</a></li>              
+          </ul>
+        </li>
+        <!-------------------------- Slider -------------------------->
+        <li class="br-menu-item">
+          <a href="#" class="br-menu-link with-sub">
+            <i class="menu-item-icon icon ion-ios-photos-outline tx-20"></i>
+            <span class="menu-item-label">Slider</span>
+          </a><!-- br-menu-link -->
+          <ul class="br-menu-sub">
+            <li class="sub-item"><a href="" class="sub-link">Manage Slider</a></li>  
+            <li class="sub-item"><a href="" class="sub-link">Add Slider</a></li>              
+          </ul>
+        </li>
+        <!-------------------------- Coupon -------------------------->
+        <li class="br-menu-item">
+          <a href="#" class="br-menu-link with-sub">
+            <i class="menu-item-icon icon ion-ios-photos-outline tx-20"></i>
+            <span class="menu-item-label">Coupon</span>
+          </a><!-- br-menu-link -->
+          <ul class="br-menu-sub">
+            <li class="sub-item"><a href="" class="sub-link">Manage Coupon</a></li>  
+            <li class="sub-item"><a href="" class="sub-link">Add Coupon</a></li>              
+          </ul>
+        </li>
+        <!-------------------------- Review -------------------------->
+        <li class="br-menu-item">
+          <a href="#" class="br-menu-link with-sub">
+            <i class="menu-item-icon icon ion-ios-photos-outline tx-20"></i>
+            <span class="menu-item-label">Review</span>
+          </a><!-- br-menu-link -->
+          <ul class="br-menu-sub">
+            <li class="sub-item"><a href="" class="sub-link">Manage Review</a></li>           
+          </ul>
+        </li>
+        <!-------------------------- Comments -------------------------->
+        <li class="br-menu-item">
+          <a href="#" class="br-menu-link with-sub">
+            <i class="menu-item-icon icon ion-ios-photos-outline tx-20"></i>
+            <span class="menu-item-label">Comments</span>
+          </a><!-- br-menu-link -->
+          <ul class="br-menu-sub">
+            <li class="sub-item"><a href="" class="sub-link">Manage comments</a></li>           
+          </ul>
+        </li>
+        <!-------------------------- Message -------------------------->
+        <li class="br-menu-item">
+          <a href="#" class="br-menu-link with-sub">
+            <i class="menu-item-icon icon ion-ios-photos-outline tx-20"></i>
+            <span class="menu-item-label">Message</span>
+          </a><!-- br-menu-link -->
+          <ul class="br-menu-sub">
+            <li class="sub-item"><a href="" class="sub-link">Massage</a></li>           
+          </ul>
+        </li>
+        
+      <br>
+    </div><!-- br-sideleft -->
+    <!-- ########## END: LEFT PANEL ########## -->
